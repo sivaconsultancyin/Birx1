24701edc9f002885a7924c74ee9e67112ede6b52
Siva consultancy <sivaconsultancy.in@gmail.com>
2026-09-20 15:24:25 +0530

    fix: import Express NextFunction for auth middleware

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import crypto from 'node:crypto';
 import path from 'path';
 import { createServer as createViteServer } from 'vite';
